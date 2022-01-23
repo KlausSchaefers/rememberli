0104a0d25098f72f1fdef1eed6aa68f349c31cf2
KlausSchaefersAtWork <klaus.schaefers@philips.com>
2022-01-23 18:27:38 +0000

    CheckBoxList

--- src/components/SettingsDialog.vue
+++ src/components/SettingsDialog.vue
@@ -9,12 +9,14 @@
         <RadioList :options="sizes" v-model="settings.fontSize"/>
 
         <h1 class="rmli-margin-top-l">{{$t('settings.other')}}</h1>
-        <CheckBox v-model="settings.hasDueInTop" :label="$t('settings.hasDueInTop')"/>
-        <CheckBox v-model="settings.hasDueFolder" :label="$t('settings.hasDueFolder')"/>
-        <CheckBox v-model="settings.hasTimeline" :label="$t('settings.timeline')"/>
-        <CheckBox v-model="settings.hasBorderTop" :label="$t('settings.hasBorderTop')"/>
-        <CheckBox v-model="settings.hasDateLeft" :label="$t('settings.hasDateLeft')"/>
-        <CheckBox v-if="false" v-model="settings.hasBeta" :label="$t('settings.beta')"/>
+        <div class="rmli-checkbox-list">
+          <CheckBox v-model="settings.hasDueInTop" :label="$t('settings.hasDueInTop')"/>
+          <CheckBox v-model="settings.hasDueFolder" :label="$t('settings.hasDueFolder')"/>
+          <CheckBox v-model="settings.hasTimeline" :label="$t('settings.timeline')"/>
+          <CheckBox v-model="settings.hasBorderTop" :label="$t('settings.hasBorderTop')"/>
+          <CheckBox v-model="settings.hasDateLeft" :label="$t('settings.hasDateLeft')"/>
+          <CheckBox v-if="false" v-model="settings.hasBeta" :label="$t('settings.beta')"/>
+        </div>
 
         
 
